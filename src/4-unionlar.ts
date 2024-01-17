9a8f8435c73d887a53c60dfbfd9dad349fe54642
Beyza Nur Karadana <83921230+BeyzaNurKaradana@users.noreply.github.com>
2024-01-17 22:33:01 +0300

    completed unions

--- src/4-unionlar.ts
+++ src/4-unionlar.ts
@@ -4,24 +4,20 @@
 // "highScore" isminde bir değişken oluşturalım. Bu değişken, bir sayı veya
 // boolean değer alabilsin.
 
-
 // Kodu buraya yazalım...
 
-
-
+let highScore: number | boolean;
 
 // **********************************************
 // ******************* BÖLÜM 2 ******************
 // **********************************************
 // "stuff" isminde boş bir array oluşturalım. Bu array, sayılardan veya stringlerden
-// oluşabilsin. Ancak sayılar ve string'ler bu array'in içinde bir arada olamasın. 
+// oluşabilsin. Ancak sayılar ve string'ler bu array'in içinde bir arada olamasın.
 // Yani ya sayı array'i olabilsin, ya da string array'i olabilsin.
-
 
 // Kodu buraya yazalım...
 
-
-
+let stuff: string[] | number[];
 
 // **********************************************
 // ******************* BÖLÜM 3 ******************
@@ -29,11 +25,8 @@
 // "SkillLevel" isminde bir literal türü oluşturalım (enum değil, literal tür).
 // Bu tür, "Beginner", "Intermediate", "Advanced", ve "Expert" değerlerini alabilsin.
 
-
 // Kodu buraya yazalım...
-
-
-
+type SkillLevel = "Intermediate" | "Advanced" | "Expert";
 
 // **********************************************
 // ******************* BÖLÜM 4 ******************
@@ -45,11 +38,14 @@
 // - sport değeri "ski" veya "snowboard" olsun
 // - level ise yukarıda oluşturduğumuz SkillLevel türünden bir değer olsun
 
-
 // Kodu buraya yazalım...
 
-
-
+type SkiSchoolStudent = {
+  name: string;
+  age: number;
+  sport: "ski" | "snowboard";
+  Level: SkillLevel;
+};
 
 // **********************************************
 // ******************* BÖLÜM 5 ******************
@@ -60,10 +56,14 @@
 // - g bir numara olsun
 // - b bir numara olsun
 
-
 // Kodu buraya yazalım...
 
+type RGB = {
+    r: number,
+    g: number,
+    b: number,
 
+};
 
 // HSL renklerini temsil eden bir tür oluşturalım. Bu tür, aşağıdaki özelliklere sahip
 // olsun:
@@ -71,20 +71,23 @@
 // - s bir numara olsun
 // - l bir numara olsun
 
-
 // Kodu buraya yazalım...
 
+type HSL = {
+    h: number,
+    s: number,
+    l: number,
 
+};
 
-// "colors" isminde boş bir array oluşturalım. Bu array, yukarıda oluşturduğumuz iki 
-// türden oluşabilsin. Yani bu array'in içinde hem RGB renkler, hem de HSL renkler 
+// "colors" isminde boş bir array oluşturalım. Bu array, yukarıda oluşturduğumuz iki
+// türden oluşabilsin. Yani bu array'in içinde hem RGB renkler, hem de HSL renkler
 // bulunabilsin.
 
-
 // Kodu buraya yazalım...
 
-
-
+type color = RGB | HSL;
+let colors = [], color;
 
 // **********************************************
 // ******************* BÖLÜM 6 ******************
@@ -98,5 +101,15 @@
 // array girilirse, fonksiyon bu array'in her bir string elemanı için ayrı ayrı
 // "Merhaba, İSİM" şeklinde bir string yazdırsın.
 
-
 // Kodu buraya yazalım...
+
+
+function greet(names: string | string[]) {
+    if (typeof names === "string") {
+      console.log(`Merhaba, ${names}!`);
+    } else {
+      for (const name of names) {
+        console.log(`Merhaba, ${name}!`);
+      }
+    }
+  }
