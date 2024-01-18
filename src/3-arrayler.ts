8adf5d6fc3b29e2c33ed3afcfd537ff67576e747
Beyza Nur Karadana <83921230+BeyzaNurKaradana@users.noreply.github.com>
2024-01-18 19:13:50 +0300

    modified arrays

--- src/3-arrayler.ts
+++ src/3-arrayler.ts
@@ -4,12 +4,9 @@
 // Yaşları tutmak üzere "ages" isminde boş bir array oluşturalım.
 // Tür girmeyi unutmayalım.
 
-
 // Kodu buraya yazalım...
 /* let ages = []:number */
 let ages: number[] = [];
-
-
 
 // **********************************************
 // ******************* BÖLÜM 2 ******************
@@ -18,13 +15,9 @@ let ages: number[] = [];
 // iki boyutlu bir array olmalı. Yani array'in içinde array'ler olacak ve bu
 // array'lerin içinde string'ler olacak.
 
-
 // Kodu buraya yazalım...
 
 let gameBoard: string[][] = [];
-
-
-
 
 // **********************************************
 // ******************* BÖLÜM 3 ******************
@@ -33,11 +26,20 @@ let gameBoard: string[][] = [];
 // isimli iki property'ye sahip olsun. Örnek:
 // {name: "coffee mug", price: 11.50}
 
-
 // Kodu buraya yazalım...
 
+type Product = {
+  name: string;
+  price: number;
+};
 
+const coffeeMug = {
+  name: "coffee mug",
+  price: 11.5,
+};
 
+console.log(coffeeMug.name);
+console.log(coffeeMug.price);
 
 // **********************************************
 // ******************* BÖLÜM 4 ******************
@@ -46,5 +48,21 @@ let gameBoard: string[][] = [];
 // yazdığımız "Product" türünden oluşan bir array alsın ve bu array'in içindeki
 // ürünlerin fiyatlarının toplamını dönsün.
 
-
 // Kodu buraya yazalım...
+
+function getTotal(products: Product[]): number {
+    let totalPrice = 0;
+    for (const product of products) {
+      totalPrice += product.price;
+    }
+  
+    return totalPrice;
+  }
+  
+  const products = [
+    { name: "coffee mug", price: 11.50 },
+    { name: "t-shirt", price: 25 },
+    { name: "laptop", price: 1500 },
+  ];
+  
+  console.log(getTotal(products)); 
